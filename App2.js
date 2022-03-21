9ddf105146641fa410cef4caf64a767e9dd896a2
Adan <adancondoric@gmail.com>
2022-03-21 16:45:07 -0400

    first commit

---
+++ App2.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { YellowBox } from 'react-native';
+import EventList from './lib/listDate/EventList';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <EventList />
+    );
+  }
+}
