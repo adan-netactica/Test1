86f2a346f3e3be37f1864b200a0a8e7ae7f839e1
Adan <adancondoric@gmail.com>
2022-03-30 16:01:42 -0400

    navigation

--- App2.js
+++ App2.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import { YellowBox } from 'react-native';
-import ItemList from './lib/ListItem/ItemList';
+//import ItemList from './lib/ListItem/ItemList';
+//import LoginScreen from './lib/Screen/LoginScreen';
+
+import { StackNavigator } from 'react-navigation';
+import LoginScreen from './lib/Auth/LoginScreen';
+import ListItem from './lib/ListItem/ItemList';
 
 export default class App extends React.Component {
   render() {
     return (
-      <ItemList />
+      <LoginScreen />
       
     );
   }
--- index.js
+++ index.js
@@ -3,7 +3,9 @@
  */
 
 import {AppRegistry} from 'react-native';
-import App from './App2';
+//import App from './App2';
+//import AppProjet from './AppProject'
 import {name as appName} from './app.json';
+import Principal2 from './Principal2';
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () => Principal2);
